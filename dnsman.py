47465ef3fa81bd3384379702122f1d5ca1c483e1
abi <abysemail@zohomail.in>
2024-06-26 21:07:06 +0530

    Update dnsman.py removed unnessery things

--- dnsman.py
+++ dnsman.py
@@ -218,7 +218,7 @@ def ask_by_cli():
 if __name__ == "__main__":
 
     import argparse
-    parser = argparse.ArgumentParser(description="this will search google for query and reads   targets websites url and saves html data in json format ")
+    parser = argparse.ArgumentParser(description=" it can check is possibility of dns takeover. you can give domain name as input")
     parser.add_argument("-d", "--domain", help = "which domain you want to check like google.com")
     parser.add_argument("-in", "--input", help = "path to domain.txt contains a domain list ")
     parser.add_argument("-out", "--output", help="to which file it should write successfull takeover details")
